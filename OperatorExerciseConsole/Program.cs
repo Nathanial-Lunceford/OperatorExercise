98e64880a2db87942124fa43531f00d305c79696
Nate <nathanial.lunceford@gmail.com>
2022-07-15 15:26:20 -0500

    Initial Commit

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 17;
+        int b = 4;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        if (a == 17 && b == 4)
+        {
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+        }
+    }
+}
